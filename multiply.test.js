736a24c547f159542dd3637d5d66ca1b046940e1
Kevin // Zirconics <56792130+Zirconics@users.noreply.github.com>
2023-01-22 12:55:19 +0100

    added multiply with tests

---
+++ multiply.test.js
@@ -0,0 +1,11 @@
+const multiply = require('./multiply');
+
+test('one times two is two', () => {
+    expect(multiply(1,2)).toBe(2);
+    
+});
+
+test('five times five is 25', () => {
+    expect(sum(1,2)).toBe(25);
+    
+})
--- sum.test.js
+++ sum.test.js
@@ -2,4 +2,10 @@ const sum = require('./sum');
 
 test('one and two is three', () => {
     expect(sum(1,2)).toBe(3);
+    
 });
+
+test('one and nine is ten', () => {
+    expect(sum(1,2)).toBe(10);
+    
+})
